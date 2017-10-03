7d317844cb0a9ff968cc105cc57f0935e334d4b2
Ervin Mamutov <employervin@gmail.com>
2017-10-03 20:09:44 +0100

    Displaying mock dashboard user messages

--- Encrypic/Views/DashboardView.xaml.cs
+++ Encrypic/Views/DashboardView.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,44 @@ namespace Encrypic.Views
     /// </summary>
     public sealed partial class DashboardView : Page
     {
+        // The list of colors won't change after it's populated, so we use List<T>. 
+        // If the data can change, we should use an ObservableCollection<T> intead.
+        List<NamedColor> NamedColors = new List<NamedColor>();
+
         public DashboardView()
         {
             this.InitializeComponent();
+
+            // Use reflection to get all the properties of the Colors class.
+            IEnumerable<PropertyInfo> propertyInfos = typeof(Colors).GetRuntimeProperties();
+
+            // For each property, create a NamedColor with the property name (color name),
+            // and property value (color value). Add it the NamedColors list.
+            for (int i = 0; i < propertyInfos.Count(); i++)
+            {
+                NamedColors.Add(new NamedColor(propertyInfos.ElementAt(i).Name,
+                                    (Color)propertyInfos.ElementAt(i).GetValue(null)));
+            }
+
+            colorsListView.ItemsSource = NamedColors;
+        }
+    }
+
+    public class NamedColor
+    {
+        public NamedColor(string colorName, Color colorValue)
+        {
+            Name = colorName;
+            Color = colorValue;
+        }
+
+        public string Name { get; set; }
+
+        public Color Color { get; set; }
+
+        public SolidColorBrush Brush
+        {
+            get { return new SolidColorBrush(Color); }
         }
     }
 }
