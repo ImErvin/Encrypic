de00752f8f5c79d992b6a924f6dddbb49c4028a5
Ervin Mamutov <erviin@live.ie>
2017-12-10 19:03:44 +0000

    Added configuration for database

--- EncrypicAPI/server.js
+++ EncrypicAPI/server.js
@@ -5,3 +5,37 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to db ' + config.database);
+});
+mongoose.connection.on('error', (err) => {
+    console.log('db error ' + err);
+});
+
+const app = express();
+
+
+const users = require('./routes/User');
+
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Encrypic');
+});
+
+//run the app and listen on the port we assigned
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
